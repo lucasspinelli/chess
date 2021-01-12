98a7e65cc9d9cce8f97ec055eeb93544e7ae443f
Lucas Levi <lucas.lsl12@gmail.com>
2021-01-12 13:46:52 -0300

    Making Chess layer and printing the board like an UI made out of prompt

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,13 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        Board board = new Board (8,8);//Chess table 
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces()); //to print the board User Interface
     }
 }
 
