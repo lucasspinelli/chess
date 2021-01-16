19577b6ef85a3ef5cc3b86aa72c97009419bd523
Lucas Levi <lucas.lsl12@gmail.com>
2021-01-16 16:40:32 -0300

    Validating if have an oponent piece in the target move

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece { //abstract becausa whe dont know the move Method, because ChessPiece could be anyone
     private Color color;
@@ -9,6 +10,11 @@ public abstract class ChessPiece extends Piece { //abstract becausa whe dont kno
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
+    }
+
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);// p receives the position, with downcasting to work
+        return p != null && p.getColor() != color; // If is an opposite color, will return true.
     }
 
     public Color getColor() { //just for access and not to chance
