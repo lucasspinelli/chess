24d482fc4df9997cc2b4edcc8ea3b59b4b63f919
Lucas Levi <lucas.lsl12@gmail.com>
2021-01-16 16:54:27 -0300

    Possible moves to Rocks

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,7 +18,49 @@ public class Rook extends ChessPiece {
     public boolean[][] possibleMoves() {
         // initial matrix starting with every position FALSE and same dimensions of the board
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-        System.out.println(mat);
+
+        Position p = new Position(0, 0);
+
+        //above
+        p.setValues(position.getRow() - 1, position.getColumn());
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true; // Changing the matrix value to true, for the Piece can move itself
+            p.setRow(p.getRow() - 1); // to go up until theres empty places
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true; // Go up until find an enemy piece
+        }
+
+        //left
+        p.setValues(position.getRow() , position.getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true; // Changing the matrix value to true, for the Piece can move itself
+            p.setColumn(p.getColumn() - 1); // to go left until theres empty places
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true; // Go left until find an enemy piece
+        }
+
+        //right
+        p.setValues(position.getRow() , position.getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true; // Changing the matrix value to true, for the Piece can move itself
+            p.setColumn(p.getColumn() + 1); // to go left until theres empty places
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true; // Go left until find an enemy piece
+        }
+
+        //below
+        p.setValues(position.getRow() + 1, position.getColumn());
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true; // Changing the matrix value to true, for the Piece can move itself
+            p.setRow(p.getRow() + 1); // to go up until theres empty places
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true; // Go up until find an enemy piece
+        }
+
         return mat;
 
     }
