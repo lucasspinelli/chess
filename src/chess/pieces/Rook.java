2a00b94ce2c201e26e52d4f9aae4a82c28cb8372
Lucas Levi <lucas.lsl12@gmail.com>
2021-01-16 13:19:40 -0300

    Implement possible moves basic logic

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -13,4 +13,12 @@ public class Rook extends ChessPiece {
     public String toString() {
         return "R"; //piece that will be showed in terminal
     }
+    @Override //implement move method
+    public boolean[][] possibleMoves() {
+        // initial matrix starting with every position FALSE and same dimensions of the board
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        System.out.println(mat);
+        return mat;
+
+    }
 }
