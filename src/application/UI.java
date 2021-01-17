17da1dc0eec653037a4f7a3b6ec8cbde1d421e22
Lucas Levi <lucas.lsl12@gmail.com>
2021-01-17 20:45:35 -0300

    Changin turns and players

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -32,6 +33,13 @@ public class UI {
         catch (RuntimeException e){
             throw new InputMismatchException("Error reading chess position"); // native input error from Java
         }
+    }
+
+    public static void printMatch(ChessMatch chessMatch){ //show the turn data
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
