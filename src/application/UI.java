6b612912df496a72b95d4f6ad72c49e7ad985415
Lucas Levi <lucas.lsl12@gmail.com>
2021-01-17 01:29:16 -0300

    Resolving null valor to position

--- src/application/UI.java
+++ src/application/UI.java
@@ -14,10 +14,9 @@ public class UI {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_BLUE = "\u001B[34m";
 
-    //scr = https://dicasdejava.com.br/java-como-limpar-a-tela-do-console/#:~:text=Para%20limpar%20a%20tela%20do,e%20executar%20o%20respectivo%20comando.
-    public static void clearScreen(){
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        public static void clearScreen(){
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
     }
 
     public static ChessPosition readChessPosition(Scanner sc){
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,28 +1,29 @@
 package boardgame;
 
 public abstract class Piece {
+
     protected Position position;
     private Board board;
 
-    public Piece(Board board) { //constructor just with board, because Piece first position is null
+    public Piece(Board board) {
         this.board = board;
         position = null;
     }
 
-    protected Board getBoard() { // just getter, the board will not change
-
+    protected Board getBoard() {
         return board;
     }
-    public abstract boolean[][] possibleMoves(); //We will put every possible move as TRUE and the other places will be FALSE
+
+    public abstract boolean[][] possibleMoves();
 
     public boolean possibleMove(Position position) {
-        return possibleMoves()[position.getRow()][position.getColumn()];//hook method = when concrete method returns an abstract one
+        return possibleMoves()[position.getRow()][position.getColumn()];
     }
 
     public boolean isThereAnyPossibleMove() {
         boolean[][] mat = possibleMoves();
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat.length; j++) {
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat.length; j++) {
                 if (mat[i][j]) {
                     return true;
                 }
@@ -31,6 +32,7 @@ public abstract class Piece {
         return false;
     }
 }
+
 /* To avoid errors and protect the board and pieces in
 * the middle of the game, I put piece and board protected
 * I'll just change that in boardgame package and subclasses
